b91b4c627eec834a81c5403a91214c908cb8c36f
rgsk <guptarahul70322@gmail.com>
2024-05-04 03:03:02 +0530

    using langchain instead of oai for func calling

--- src/navigate_skartner/navigate_skartner.py
+++ src/navigate_skartner/navigate_skartner.py
@@ -1,58 +1,14 @@
-import json
+from typing import Any
+
+from langchain_anthropic import ChatAnthropic
 
 from src.navigate_skartner.tools import tools
-from src.openai_client import openai_client
 
-GPT_MODEL = "gpt-3.5-turbo-0613"
+llm = ChatAnthropic(model="claude-3-sonnet-20240229")  # type: ignore
 
-
-def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
-    try:
-        response = openai_client.chat.completions.create(
-            model=model,
-            messages=messages,
-            tools=tools,
-            tool_choice=tool_choice,
-        )
-        return response
-    except Exception as e:
-        print("Unable to generate ChatCompletion response")
-        print(f"Exception: {e}")
-        return e
-
-
-messages = []
-system_message = {
-    "role": "system",
-    "content": "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."
-}
-
-
-def use_assistant_message(messages, assistant_message):
-    if assistant_message.tool_calls:
-        for tool_call in assistant_message.tool_calls:
-            messages.append({"role": "function", "tool_call_id": tool_call.id,
-                            "name": tool_call.function.name, "arguments": tool_call.function.arguments,  "content": ""})
-    else:
-        return {"role": assistant_message.role, "content": assistant_message.content}
-
-
-def pop_arguments_key(messages):
-    new_messages = json.loads(json.dumps(messages))
-    for message in new_messages:
-        if 'arguments' in message:
-            message.pop('arguments')
-    return new_messages
+llm_with_tools = llm.bind_tools(tools)
 
 
 def navigate_skartner(user_message: str):
-    global messages
-    messages.append({"role": "user", "content": user_message})
-    # print(len(messages))
-    chat_response = chat_completion_request(
-        pop_arguments_key([system_message] + messages), tools=tools
-    )
-    assistant_message = chat_response.choices[0].message
-    use_assistant_message(messages, assistant_message)
-    messages = messages[-4:]
-    return assistant_message
+    result: Any = llm_with_tools.invoke(user_message)
+    return result
--- src/navigate_skartner/tools.py
+++ src/navigate_skartner/tools.py
@@ -1,109 +1,68 @@
-tools = [
-    {
-        "type": "function",
-        "function": {
-            "name": "searchWord",
-            "description": "Search a word or to get meaning of a word or to run a prompt against a given word",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "word": {
-                        "type": "string",
-                        "description": "the word we want to search",
-                    },
-                },
-                "required": ["word"],
-            },
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "saveWord",
-            "description": "Save the word in the dictionary",
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "goToManagePrompts",
-            "description": "manage prompts or click manage prompts or go to manage prompts page",
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "addNewPrompt",
-            "description": "start creating a new prompt or add new prompt, if user has specified the prompt then run the type tool next with prompt specified to type the prompt",
-           
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "editPrompt",
-            "description": "start editing a prompt from a list of prompts",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "promptId": {
-                        "type": "string",
-                        "description": "the prompt id we want to edit",
-                    },
-                },
-                "required": ["promptId"],
-            },
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "type",
-            "description": "type with keyboard",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "value": {
-                        "type": "string",
-                        "description": "what user wanted to type",
-                    },
-                },
-                "required": ["value"],
-            },
-        }
-    },
-   {
-        "type": "function",
-        "function": {
-            "name": "saveThePrompt",
-            "description": "save the prompt that we have typed",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "prompt": {
-                        "type": "string",
-                        "description": "the prompt we want to save, must contain word enclosed in curly brackets like {word}, eg 'list meaning of word {word}, 3 example sentences and antonyms and synonyms', you have to ensure that you add placeholder like {word}",
-                    },
-                },
-                "required": ["prompt"],
-            },
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "selectPromptAsDefault",
-            "description": "select one of the prompts from a list of prompts as default",
-             "parameters": {
-                "type": "object",
-                "properties": {
-                    "promptId": {
-                        "type": "string",
-                        "description": "the prompt id we want to set as default",
-                    },
-                },
-                "required": ["promptId"],
-            },
-        }
-    }
-]
+from typing import Any, List
+
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class searchWord(BaseModel):
+    """Search a word or to get meaning of a word or to run a prompt against a given word"""
+
+    word: str = Field(description="the word we want to search")
+
+
+class saveWord(BaseModel):
+    """Save the word in the dictionary"""
+
+
+class goToManagePrompts(BaseModel):
+    """manage prompts or click manage prompts or go to manage prompts page"""
+
+
+valid_prompt_description = """
+the prompt we want to save, the prompt must contain word enclosed in curly brackets like {word}, this will act as a placeholder where we can pass the word when we are searching for meaning of a word,
+eg. if user specified "list meaning of word and 3 examples", the prompt that you will pass to the function is "list meaning of word {word} and 3 examples" or "list meaning of {word} and 3 examples"
+"""
+
+
+class addNewPrompt(BaseModel):
+    """start creating a new prompt or add new prompt, if user has specified the prompt then pass the prompt as an argument, otherwise empty string"""
+    prompt: str = Field(description=valid_prompt_description)
+
+
+class saveThePrompt(BaseModel):
+    """save the prompt that user has provided"""
+    prompt: str = Field(description=valid_prompt_description)
+
+
+class typeWithKeyboard(BaseModel):
+    """when user says type or type with keyboard"""
+    value: str = Field(description="what user wanted to type")
+
+
+how_to_locate_prompt_from_list = """
+    Here's more details about prompt and how to locate it from a list of prompts
+    note: that prompt has three fields id: str, text: str and default: bool,
+    you will be given a list of prompts, your job is to return the id of prompt that user intends to edit
+    user can ask you to edit in several ways for eg. 
+    1. edit the first prompt, in this case you have to return the id of first prompt
+    2. user can say edit the default prompt, in this case return the prompt id whose default is true
+    3. user can mention a partial prompt and your job is to determine which prompts from the list matches most with the prompt mentioned by user and return id of that
+"""
+
+
+class editPrompt(BaseModel):
+    f"""start editing a prompt from a list of prompts,
+    f{how_to_locate_prompt_from_list}
+    """
+    promptId: str = Field(description="the prompt id we want to edit")
+
+
+class selectPromptAsDefault(BaseModel):
+    f"""select one of the prompts from a list of prompts as default,
+    f{how_to_locate_prompt_from_list}
+    """
+    promptId: str = Field(
+        description="the prompt id we want to set as default")
+
+
+tools: List[Any] = [searchWord, saveWord, goToManagePrompts, addNewPrompt,
+                    saveThePrompt, typeWithKeyboard, editPrompt, selectPromptAsDefault]
